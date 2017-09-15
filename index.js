381fd9aca1c133df7fc8b14abc22a8ee47357d8c
AlexKvazos <contact@alexkvazos.com>
2017-09-15 11:49:03 -0500

    Update sensor to send webhooks directly

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
 var j5 = require('johnny-five');
-var io = require('socket.io-client');
+var agent = require('agent');
 board = new j5.Board({ repl: false });
 var previousValue = 0;
 
-const socket = io(process.env.API_HOST);
+const { SLACK_WEBHOOK } = process.env;
 
 /**
  * Send a request to the server to mark La Fábrica as Open
  */
 function setOpen() {
   console.log('La fabrica is open');
-  socket.emit('update', true);
+  agent.post(SLACK_WEBHOOK).send({
+    attachments: [
+      {
+        color: '#7ed510',
+        title: 'La Fábrica is now open!',
+        ts: Date.now()/1000
+      }
+    ]
+  })
+  .end((err, res) => {
+    if (err) return logger.error(err);
+  });
 }
 
 /**
  * Send a request to the server to mark La Fábrica as Closed
  */
 function setClosed() {
-console.log('La fabrica is closed');
-socket.emit('update', false);
+  console.log('La fabrica is closed');
+  agent.post(SLACK_WEBHOOK).send({
+      attachments: [
+        {
+          color: '#F23E0F',
+          title: 'La Fábrica is now closed!',
+          ts: Date.now()/1000
+        }
+      ]
+    })
+    .end((err, res) => {
+      if (err) return logger.error(err);
+    });
 }
 
 /**
